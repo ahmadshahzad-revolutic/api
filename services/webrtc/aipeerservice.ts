2ba20369cc104a89e64724af17370d184bf21721
ahmad6522dev <ahmad6522.dev@gmail.com>
2026-02-20 14:22:11 +0500

    solve build errors

--- services/webrtc/aipeerservice.ts
+++ services/webrtc/aipeerservice.ts
@@ -41,8 +41,6 @@ export function createAIPeerService(callId: string = 'default'): AIPeerService {
             },
             iceServers: [
                 { urls: "stun:stun.l.google.com:19302" },
-                { urls: "turn:free.expressturn.com:3478", username: "000000002086350036", credential: "Vze+mrSSNkkOxbV7RgqDBs3P8gg=" },
-                { urls: "turn:free.expressturn.com:3478?transport=tcp", username: "000000002086350036", credential: "Vze+mrSSNkkOxbV7RgqDBs3P8gg=" },
             ]
         }),
         stt: createSTTService('auto'),
